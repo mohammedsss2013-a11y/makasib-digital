af7d232c534c9d50b24ed842e5ec40e24c58a67a
Amir <amir@example.com>
2026-08-11 04:00:28 +0300

    feat: upload actual site pages and components

--- app/push.bat
+++ app/push.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d C:\Users\AMIR\.gemini\antigravity\scratch\makasib-digital\makasib-digital
 git add .
-git commit -m "feat: update project and components"
+git commit -m "feat: upload actual site pages and components"
 git push origin main
 pause
