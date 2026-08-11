e8089758a774f41b3382dfafb3adaaa595ea4952
Amir <amir@example.com>
2026-08-11 03:39:57 +0300

    feat: update project and components

---
+++ push.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d C:\Users\AMIR\.gemini\antigravity\scratch\makasib-digital\makasib-digital
+git add .
+git commit -m "feat: update project and components"
+git push origin main
+pause
